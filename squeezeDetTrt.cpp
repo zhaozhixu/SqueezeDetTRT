5849702ade5e0f3824d4e2f741c70eeda3292486
Zhao Zhixu <zzx.2013@qq.com>
2017-11-06 19:55:52 +0800

    conv layer finished

--- squeezeDetTrt.cpp
+++ squeezeDetTrt.cpp
@@ -4,14 +4,19 @@
 #include <iostream>
 #include <cmath>
 #include <sys/stat.h>
-#include <cmath>
+#include <sys/types.h>
 #include <time.h>
 #include <cuda_runtime_api.h>
 #include <cudnn.h>
 #include <cublas_v2.h>
 #include <memory>
 #include <cstring>
+#include <cstdlib>
 #include <algorithm>
+#include <opencv2/opencv.hpp>
+#include <cstdio>
+#include <dirent.h>
+#include <unistd.h>
 
 #include "NvCaffeParser.h"
 #include "NvInferPlugin.h"
@@ -35,11 +40,10 @@ static const int NUM_OUTPUT = ANCHOR_PER_GRID * (OUTPUT_CLS_SIZE + 1 + 4);
 const std::string CLASSES[OUTPUT_CLS_SIZE]{ "car", "pedestrian", "cyclist" };
 
 const char* INPUT_BLOB_NAME0 = "data";
-const char* INPUT_BLOB_NAME1 = "im_info";
-const char* OUTPUT_BLOB_NAME0 = "bbox_pred";
-const char* OUTPUT_BLOB_NAME1 = "cls_prob";
-const char* OUTPUT_BLOB_NAME2 = "rois";
-
+// const char* INPUT_BLOB_NAME1 = "im_info";
+const char* OUTPUT_BLOB_NAME0 = "bbox_delta";
+const char* OUTPUT_BLOB_NAME1 = "pred_class_probs";
+const char* OUTPUT_BLOB_NAME2 = "pred_confidence_score";
 
 const int poolingH = 7;
 const int poolingW = 7;
@@ -154,52 +158,6 @@ void writePPMFileWithBBox(const std::string& filename, PPM ppm, BBox bbox)
 	outfile.write(reinterpret_cast<char*>(ppm.buffer), ppm.w * ppm.h * 3);
 }
 
-void caffeToGIEModel(const std::string& deployFile,			// name for caffe prototxt
-	const std::string& modelFile,			// name for model
-	const std::vector<std::string>& outputs,		// network outputs
-	unsigned int maxBatchSize,				// batch size - NB must be at least as large as the batch we want to run with)
-	nvcaffeparser1::IPluginFactory* pluginFactory,	// factory for plugin layers
-	IHostMemory **gieModelStream)			// output stream for the GIE model
-{
-	// create the builder
-	IBuilder* builder = createInferBuilder(gLogger);
-
-	// parse the caffe model to populate the network, then set the outputs
-	INetworkDefinition* network = builder->createNetwork();
-	ICaffeParser* parser = createCaffeParser();
-	parser->setPluginFactory(pluginFactory);
-
-	std::cout << "Begin parsing model..." << std::endl;
-	const IBlobNameToTensor* blobNameToTensor = parser->parse(locateFile(deployFile).c_str(),
-		locateFile(modelFile).c_str(),
-		*network,
-		DataType::kFLOAT);
-	std::cout << "End parsing model..." << std::endl;
-	// specify which tensors are outputs
-	for (auto& s : outputs)
-		network->markOutput(*blobNameToTensor->find(s.c_str()));
-
-	// Build the engine
-	builder->setMaxBatchSize(maxBatchSize);
-	builder->setMaxWorkspaceSize(10 << 20);	// we need about 6MB of scratch space for the plugin layer for batch size 5
-
-	std::cout << "Begin building engine..." << std::endl;
-	ICudaEngine* engine = builder->buildCudaEngine(*network);
-	assert(engine);
-	std::cout << "End building engine..." << std::endl;
-
-	// we don't need the network any more, and we can destroy the parser
-	network->destroy();
-	parser->destroy();
-
-	// serialize the engine, then close everything down
-	(*gieModelStream) = engine->serialize();
-
-	engine->destroy();
-	builder->destroy();
-	shutdownProtobufLibrary();
-}
-
 IConvolutionLayer*
 addFireLayer(INetworkDefinition* network, ITensor& input, int ns1x1, int ne1x1, int ne3x3,
              Weights wks1x1, Weights wke1x1, Weights wke3x3,
@@ -218,13 +176,17 @@ addFireLayer(INetworkDefinition* network, ITensor& input, int ns1x1, int ne1x1, 
      auto relu2 = network->addActivation(*ex1x1->getOutput(0), ActivationType::kRELU);
      assert(relu2 != nullptr);
 
-     auto ex3x3 = network->addConvolution(*relu2->getOutput(0), ne3x3, DimsHW{3, 3}, wke3x3, wbe3x3);
+     auto ex3x3 = network->addConvolution(*relu1->getOutput(0), ne3x3, DimsHW{3, 3}, wke3x3, wbe3x3);
      assert(ex3x3 != nullptr);
      ex3x3->setStride(DimsHW{1, 1});
      auto relu3 = network->addActivation(*ex3x3->getOutput(0), ActivationType::kRELU);
      assert(relu3 != nullptr);
 
-     return relu3;
+	 ITensor *concatTensors[] = {relu2->getOutput(0), relu3->getOutput(0)};
+	 auto concat = network->addConcatenation(concatTensors, 2);
+	 assert(concat != nullptr);
+
+     return concat;
 }
 
 // Creat the Engine using only the API and not any parser.
@@ -233,17 +195,8 @@ createMNISTEngine(unsigned int maxBatchSize, IBuilder *builder, DataType dt)
 {
 	INetworkDefinition* network = builder->createNetwork();
 
-	//  Create input of shape { 1, 1, 28, 28 } with name referenced by INPUT_BLOB_NAME
-	auto data = network->addInput(INPUT_BLOB_NAME, dt, DimsCHW{ 1, INPUT_H, INPUT_W});
+	auto data = network->addInput(INPUT_BLOB_NAME, dt, DimsCHW{ 3, INPUT_H, INPUT_W});
 	assert(data != nullptr);
-
-	// Create a scale layer with default power/shift and specified scale parameter.
-	// float scale_param = 0.0125f;
-	// Weights power{DataType::kFLOAT, nullptr, 0};
-	// Weights shift{DataType::kFLOAT, nullptr, 0};
-	// Weights scale{DataType::kFLOAT, &scale_param, 1};
-	// auto scale_1 = network->addScale(*data,	ScaleMode::kUNIFORM, shift, scale, power);
-	// assert(scale_1 != nullptr);
 
     std::map<std::string, Weights> weightMap = loadWeights(locateFile("squeezedettrt.wts")); // ?
 	auto conv1 = network->addConvolution(*data->getOutput(0), 64, DimsHW{3, 3},
@@ -349,11 +302,8 @@ createMNISTEngine(unsigned int maxBatchSize, IBuilder *builder, DataType dt)
     assert(preds != nullptr);
     preds->setStride(DimsHW{1, 1}); // what is xavier, stddev?
 
-	// Add a softmax layer to determine the probability.
-	// auto prob = network->addSoftMax(*ip2->getOutput(0));
-	// assert(prob != nullptr);
-	// prob->getOutput(0)->setName(OUTPUT_BLOB_NAME);
-	// network->markOutput(*prob->getOutput(0));
+	prob->getOutput(0)->setName(OUTPUT_BLOB_NAME);
+	network->markOutput(*prob->getOutput(0));
 
 	// Build the engine
 	builder->setMaxBatchSize(maxBatchSize);
@@ -392,22 +342,17 @@ void doInference(IExecutionContext& context, float* inputData, float* inputImInf
 {
 	const ICudaEngine& engine = context.getEngine();
 	// input and output buffer pointers that we pass to the engine - the engine requires exactly IEngine::getNbBindings(),
-	// of these, but in this case we know that there is exactly 2 inputs and 3 outputs.
-	assert(engine.getNbBindings() == 5);
-	void* buffers[5];
+	// of these, but in this case we know that there is exactly 1 inputs and 1 outputs.
+	assert(engine.getNbBindings() == 2);
+	void* buffers[2];
 
 	// In order to bind the buffers, we need to know the names of the input and output tensors.
 	// note that indices are guaranteed to be less than IEngine::getNbBindings()
 	int inputIndex0 = engine.getBindingIndex(INPUT_BLOB_NAME0),
-		inputIndex1 = engine.getBindingIndex(INPUT_BLOB_NAME1),
 		outputIndex0 = engine.getBindingIndex(OUTPUT_BLOB_NAME0),
-		outputIndex1 = engine.getBindingIndex(OUTPUT_BLOB_NAME1),
-		outputIndex2 = engine.getBindingIndex(OUTPUT_BLOB_NAME2);
-
 
 	// create GPU buffers and a stream
 	CHECK(cudaMalloc(&buffers[inputIndex0], batchSize * INPUT_C * INPUT_H * INPUT_W * sizeof(float)));   // data
-	CHECK(cudaMalloc(&buffers[inputIndex1], batchSize * IM_INFO_SIZE * sizeof(float)));                  // im_info
 	CHECK(cudaMalloc(&buffers[outputIndex0], batchSize * nmsMaxOut * OUTPUT_BBOX_SIZE * sizeof(float))); // bbox_pred
 	CHECK(cudaMalloc(&buffers[outputIndex1], batchSize * nmsMaxOut * OUTPUT_CLS_SIZE * sizeof(float)));  // cls_prob
 	CHECK(cudaMalloc(&buffers[outputIndex2], batchSize * nmsMaxOut * 4 * sizeof(float)));                // rois
@@ -433,163 +378,6 @@ void doInference(IExecutionContext& context, float* inputData, float* inputImInf
 	CHECK(cudaFree(buffers[outputIndex1]));
 	CHECK(cudaFree(buffers[outputIndex2]));
 }
-
-template<int OutC>
-class Reshape : public IPlugin
-{
-public:
-	Reshape() {}
-	Reshape(const void* buffer, size_t size)
-	{
-		assert(size == sizeof(mCopySize));
-		mCopySize = *reinterpret_cast<const size_t*>(buffer);
-	}
-
-	int getNbOutputs() const override
-	{
-		return 1;
-	}
-	Dims getOutputDimensions(int index, const Dims* inputs, int nbInputDims) override
-	{
-		assert(nbInputDims == 1);
-		assert(index == 0);
-		assert(inputs[index].nbDims == 3);
-		assert((inputs[0].d[0])*(inputs[0].d[1]) % OutC == 0);
-		return DimsCHW(OutC, inputs[0].d[0] * inputs[0].d[1] / OutC, inputs[0].d[2]);
-	}
-
-	int initialize() override
-	{
-		return 0;
-	}
-
-	void terminate() override
-	{
-	}
-
-	size_t getWorkspaceSize(int) const override
-	{
-		return 0;
-	}
-
-	// currently it is not possible for a plugin to execute "in place". Therefore we memcpy the data from the input to the output buffer
-	int enqueue(int batchSize, const void*const *inputs, void** outputs, void*, cudaStream_t stream) override
-	{
-		CHECK(cudaMemcpyAsync(outputs[0], inputs[0], mCopySize * batchSize, cudaMemcpyDeviceToDevice, stream));
-		return 0;
-	}
-
-
-	size_t getSerializationSize() override
-	{
-		return sizeof(mCopySize);
-	}
-
-	void serialize(void* buffer) override
-	{
-		*reinterpret_cast<size_t*>(buffer) = mCopySize;
-	}
-
-	void configure(const Dims*inputs, int nbInputs, const Dims* outputs, int nbOutputs, int)	override
-	{
-		mCopySize = inputs[0].d[0] * inputs[0].d[1] * inputs[0].d[2] * sizeof(float);
-	}
-
-protected:
-	size_t mCopySize;
-};
-
-
-// integration for serialization
-class PluginFactory : public nvinfer1::IPluginFactory, public nvcaffeparser1::IPluginFactory
-{
-public:
-	// deserialization plugin implementation
-	virtual nvinfer1::IPlugin* createPlugin(const char* layerName, const nvinfer1::Weights* weights, int nbWeights) override
-	{
-		assert(isPlugin(layerName));
-		if (!strcmp(layerName, "ReshapeCTo2"))
-		{
-			assert(mPluginRshp2 == nullptr);
-			assert(nbWeights == 0 && weights == nullptr);
-			mPluginRshp2 = std::unique_ptr<Reshape<2>>(new Reshape<2>());
-			return mPluginRshp2.get();
-		}
-		else if (!strcmp(layerName, "ReshapeCTo18"))
-		{
-			assert(mPluginRshp18 == nullptr);
-			assert(nbWeights == 0 && weights == nullptr);
-			mPluginRshp18 = std::unique_ptr<Reshape<18>>(new Reshape<18>());
-			return mPluginRshp18.get();
-		}
-		else if (!strcmp(layerName, "RPROIFused"))
-		{
-			assert(mPluginRPROI == nullptr);
-			assert(nbWeights == 0 && weights == nullptr);
-			mPluginRPROI = std::unique_ptr<INvPlugin, decltype(nvPluginDeleter)>
-				(createFasterRCNNPlugin(featureStride, preNmsTop, nmsMaxOut, iouThreshold, minBoxSize, spatialScale,
-					DimsHW(poolingH, poolingW), Weights{ nvinfer1::DataType::kFLOAT, anchorsRatios, anchorsRatioCount },
-					Weights{ nvinfer1::DataType::kFLOAT, anchorsScales, anchorsScaleCount }), nvPluginDeleter);
-			return mPluginRPROI.get();
-		}
-		else
-		{
-			assert(0);
-			return nullptr;
-		}
-	}
-
-	IPlugin* createPlugin(const char* layerName, const void* serialData, size_t serialLength) override
-	{
-		assert(isPlugin(layerName));
-		if (!strcmp(layerName, "ReshapeCTo2"))
-		{
-			assert(mPluginRshp2 == nullptr);
-			mPluginRshp2 = std::unique_ptr<Reshape<2>>(new Reshape<2>(serialData, serialLength));
-			return mPluginRshp2.get();
-		}
-		else if (!strcmp(layerName, "ReshapeCTo18"))
-		{
-			assert(mPluginRshp18 == nullptr);
-			mPluginRshp18 = std::unique_ptr<Reshape<18>>(new Reshape<18>(serialData, serialLength));
-			return mPluginRshp18.get();
-		}
-		else if (!strcmp(layerName, "RPROIFused"))
-		{
-			assert(mPluginRPROI == nullptr);
-			mPluginRPROI = std::unique_ptr<INvPlugin, decltype(nvPluginDeleter)>(createFasterRCNNPlugin(serialData, serialLength), nvPluginDeleter);
-			return mPluginRPROI.get();
-		}
-		else
-		{
-			assert(0);
-			return nullptr;
-		}
-	}
-
-	// caffe parser plugin implementation
-	bool isPlugin(const char* name) override
-	{
-		return (!strcmp(name, "ReshapeCTo2")
-			|| !strcmp(name, "ReshapeCTo18")
-			|| !strcmp(name, "RPROIFused"));
-	}
-
-	// the application has to destroy the plugin when it knows it's safe to do so
-	void destroyPlugin()
-	{
-		mPluginRshp2.release();		mPluginRshp2 = nullptr;
-		mPluginRshp18.release();	mPluginRshp18 = nullptr;
-		mPluginRPROI.release();		mPluginRPROI = nullptr;
-	}
-
-
-	std::unique_ptr<Reshape<2>> mPluginRshp2{ nullptr };
-	std::unique_ptr<Reshape<18>> mPluginRshp18{ nullptr };
-	void(*nvPluginDeleter)(INvPlugin*) { [](INvPlugin* ptr) {ptr->destroy(); } };
-	std::unique_ptr<INvPlugin, decltype(nvPluginDeleter)> mPluginRPROI{ nullptr, nvPluginDeleter };
-};
-
 
 void bboxTransformInvAndClip(float* rois, float* deltas, float* predBBoxes, float* imInfo,
 	const int N, const int nmsMaxOut, const int numCls)
@@ -665,27 +453,51 @@ std::vector<int> nms(std::vector<std::pair<float, int> >& score_index, float* bb
 	return indices;
 }
 
+std::vector<std::string> getImageList(const char *pathname)
+{
+    struct stat statbuf;
+    struct dirent *dirp;
+    DIR *dp;
+    std::vector<std::string> imgList;
+
+    if (stat(pathname, &statbuf) < 0) {
+        perror("stat failed");
+        exit(EXIT_FAILURE);
+    }
+    if (S_ISDIR(statbuf.st_mode) == 0) {
+        fprintf(stderr, "'%s' not a directory.\n", pathname);
+        exit(EXIT_FAILURE);
+    }
+    if ((dp = opendir(pathname)) == NULL) {
+        perror("cannot read directory");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        if (!strcmp(dirp->d_name, ".") || !strcmp(dirp->d_name, ".."))
+            continue;
+        // TODO: filter jpeg
+        imgList.push_back(std::string(dirp->d_name));
+    }
+    return imgList;
+}
 
 int main(int argc, char** argv)
 {
-	// create a GIE model from the caffe model and serialize it to a stream
-	PluginFactory pluginFactory;
+    if (argc < 2) {
+        cout << "usage: sqzdtrt IMAGE_DIR\n";
+        exit(EXIT_SUCCESS);
+    }
+
+    std::vector<std::string> imageList = getImageList(argv[1]);
 	IHostMemory *gieModelStream{ nullptr };
 	// batch size
-	const int N = 2;
-	caffeToGIEModel("faster_rcnn_test_iplugin.prototxt",
-		"VGG16_faster_rcnn_final.caffemodel",
-		std::vector < std::string > { OUTPUT_BLOB_NAME0, OUTPUT_BLOB_NAME1, OUTPUT_BLOB_NAME2 },
-		N, &pluginFactory, &gieModelStream);
-
-	pluginFactory.destroyPlugin();
+	const int N = 20;
 	// read a random sample image
 	srand(unsigned(time(nullptr)));
 	// available images
-	std::vector<std::string> imageList = { "000456.ppm",  "000542.ppm",  "001150.ppm", "001763.ppm", "004545.ppm" };
 	std::vector<PPM> ppms(N);
 
-	float imInfo[N * 3]; // input im_info
 	std::random_shuffle(imageList.begin(), imageList.end(), [](int i) {return rand() % i; });
 	assert(ppms.size() <= imageList.size());
 	for (int i = 0; i < N; ++i)
@@ -697,8 +509,8 @@ int main(int argc, char** argv)
 	}
 
 	float* data = new float[N*INPUT_C*INPUT_H*INPUT_W];
-	// pixel mean used by the Faster R-CNN's author
-	float pixelMean[3]{ 102.9801f, 115.9465f, 122.7717f }; // also in BGR order
+	// pixel mean used by the SqueezeDet's author
+	float pixelMean[3]{ 103.939f, 116.779f, 123.68f }; // also in BGR order
 	for (int i = 0, volImg = INPUT_C*INPUT_H*INPUT_W; i < N; ++i)
 	{
 		for (int c = 0; c < INPUT_C; ++c)
