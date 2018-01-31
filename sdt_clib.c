e45045406c8245e78a887795dd3aa8aae92038d0
Zhao Zhixu <zzx.2013@qq.com>
2018-01-31 15:51:27 +0800

    finish export clib

---
+++ sdt_clib.c
@@ -0,0 +1,28 @@
+#include "sdt_clib.h"
+
+void sdt_init()
+{
+	sdt_infer_init();
+}
+
+void sdt_detect(unsigned char *input, int height, int width, int x_shift, int y_shift,
+		char *res_str, FILE *res_fp, struct predictions **res_preds)
+{
+	sdt_infer_detect(input, height, width, x_shift, y_shift,
+			res_str, res_fp, res_preds);
+}
+
+void sdt_cleanup()
+{
+	sdt_infer_cleanup();
+}
+
+float sdt_get_time_detect()
+{
+	return sdt_infer_get_time_detect();
+}
+
+float sdt_get_time_misc()
+{
+	return sdt_infer_get_time_misc();
+}
